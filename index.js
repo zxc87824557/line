7df8327e4d4522ff386b3473c2aa821e3ece95d6
zxc87824557 <61406292+zxc87824557@users.noreply.github.com>
2020-05-18 16:30:29 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -13,15 +13,68 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
-// 當收到訊息時
-bot.on('message', async (event) => {
+const getMonth = async (month) => {
+  let arr = []
   let msg = ''
   try {
-    const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
-    msg = data.entry[0].title
+    const data = await rp({ uri: 'https://bangumi.bilibili.com/web_api/timeline_global', json: true })
+    for (const d of data.result) {
+      const mon = d.date.split('-')[0]
+      if (mon === month) {
+        for (const s of d.seasons) {
+          if (!arr.includes(s.title)) arr.push(s.title)
+        }
+      }
+    }
+    msg = arr.join('\n')
   } catch (error) {
     msg = '發生錯誤'
   }
+
+  return msg
+}
+let search = ''
+let re = {}
+const getImg = async (event) => {
+  let msg = ''
+  try {
+    const data = await rp({ uri: 'https://bangumi.bilibili.com/web_api/timeline_global', json: true })
+    for (let i = 0; i < data.result.length; i++) {
+      for (let j = 0; j < data.result[i].seasons.length; j++) {
+        if (data.result[i].seasons[j].title === search) {
+          // re = {
+          //   type: 'image',
+          //   originalContentUrl: data.result[i].seasons[j].cover,
+          //   previewImageUrl: data.result[i].seasons[j].cover
+          // }
+          re = {
+            type: 'text',
+            text: data.result[i].seasons[j].url
+          }
+        }
+      }
+    }
+  } catch (error) {
+    msg = '發生錯誤'
+  }
+  // return re
+  // console.log(re)
+}
+// getImg()
+// 當收到訊息時
+bot.on('message', async (event) => {
+  search = event.message.text
+  let msg = ''
+  if (event.message.type === 'text') {
+    // const text = event.message.text
+    if (search.includes('月')) {
+      msg = await getMonth(search.substr(0, 1))
+    } else {
+      await getImg()
+      msg = re
+    }
+  }
+  console.log(msg)
   event.reply(msg)
 })
 
